52fb09001614b5b2e59122599172d0230b8e4e04
Mathis <90961553+Rinkachirikiari@users.noreply.github.com>
2024-04-15 09:31:31 -0400

    modif exp

--- experience.py
+++ experience.py
@@ -17,6 +17,36 @@ import scipy.io
 
 import torch
 import torch.nn as nn
+from torch.utils.data import Dataset
+from PIL import Image
+import torchvision.transforms as transforms
+from sklearn.decomposition import PCA
+
+import torch
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
+import torchvision.transforms as transforms
+from torchvision.utils import save_image, make_grid
+
+from torch.utils.data import DataLoader
+from torch.autograd import Variable
+import torch.autograd as autograd
+from torchvision.models import vgg19
+
+from torch import nn
+from torch import Tensor
+from PIL import Image
+from torchvision.transforms import Compose, Resize, ToTensor
+from einops import rearrange, reduce, repeat
+from einops.layers.torch import Rearrange, Reduce
+# from common_spatial_pattern import csp
+
+import matplotlib.pyplot as plt
+# from torch.utils.tensorboard import SummaryWriter
+from torch.backends import cudnn
+cudnn.benchmark = False
+cudnn.deterministic = True
 
 import transformers
 import convolution
@@ -24,10 +54,10 @@ import convolution
 
 
 class Conformer(nn.Sequential):
-    def __init__(self, emb_size=40, depth=6, n_classes=4, **kwargs):
+    def __init__(self, emb_size=40, nb_channels =23, depth=6, n_classes=2, **kwargs):
         super().__init__(
 
-            convolution.PatchEmbedding(emb_size),
+            convolution.PatchEmbedding(emb_size, nb_channels),
             transformers.TransformerEncoder(depth, emb_size),
             transformers.ClassificationHead(emb_size, n_classes)
         )
@@ -42,7 +72,7 @@ class ExP():
         self.lr = 0.0002
         self.b1 = 0.5
         self.b2 = 0.999
-        self.dimension = (190, 50) # à changer
+        self.dimension = (190, 50) # sert à quoi ? 
         self.nSub = nsub
 
         self.start_epoch = 0
@@ -59,43 +89,142 @@ class ExP():
         self.model = Conformer().cuda()
         self.model = nn.DataParallel(self.model, device_ids=[i for i in range(len(gpus))])
         self.model = self.model.cuda()
-        # summary(self.model, (1, 22, 1000))
+        
 
-    def get_source_data(self):
+    def train(self, train_data, train_label, test_data, test_label):
 
-        # train data
-        self.total_data = scipy.io.loadmat(self.root + 'A0%dT.mat' % self.nSub) # à changer
-        self.train_data = self.total_data['data']
-        self.train_label = self.total_data['label']
+        
+        train_data = torch.from_numpy(train_data)
+        train_label = torch.from_numpy(train_label - 1)
 
-        self.train_data = np.transpose(self.train_data, (2, 1, 0))
-        self.train_data = np.expand_dims(self.train_data, axis=1)
-        self.train_label = np.transpose(self.train_label)
+        dataset = torch.utils.data.TensorDataset(train_data, train_label)
+        self.dataloader = torch.utils.data.DataLoader(dataset=dataset, batch_size=self.batch_size, shuffle=True)
 
-        self.allData = self.train_data
-        self.allLabel = self.train_label[0]
+        test_data = torch.from_numpy(test_data)
+        test_label = torch.from_numpy(test_label - 1)
+        test_dataset = torch.utils.data.TensorDataset(test_data, test_label)
+        self.test_dataloader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=self.batch_size, shuffle=True)
 
-        shuffle_num = np.random.permutation(len(self.allData))
-        self.allData = self.allData[shuffle_num, :, :, :]
-        self.allLabel = self.allLabel[shuffle_num]
+        # Optimizers
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr, betas=(self.b1, self.b2))
 
-        # test data
-        self.test_tmp = scipy.io.loadmat(self.root + 'A0%dE.mat' % self.nSub)
-        self.test_data = self.test_tmp['data']
-        self.test_label = self.test_tmp['label']
+        test_data = Variable(test_data.type(self.Tensor))
+        test_label = Variable(test_label.type(self.LongTensor))
 
-        self.test_data = np.transpose(self.test_data, (2, 1, 0))
-        self.test_data = np.expand_dims(self.test_data, axis=1)
-        self.test_label = np.transpose(self.test_label)
+        bestAcc = 0
+        averAcc = 0
+        num = 0
+        Y_true = 0
+        Y_pred = 0
 
-        self.testData = self.test_data
-        self.testLabel = self.test_label[0]
+        # Train the cnn model
+        total_step = len(self.dataloader)
+        curr_lr = self.lr
 
-         # standardize
-        target_mean = np.mean(self.allData)
-        target_std = np.std(self.allData)
-        self.allData = (self.allData - target_mean) / target_std
-        self.testData = (self.testData - target_mean) / target_std
+        for e in range(self.n_epochs):
+            # in_epoch = time.time()
+            self.model.train()
+            for i, (train_data, train_label) in enumerate(self.dataloader):
 
-        # data shape: (trial, conv channel, electrode channel, time samples)
-        return self.allData, self.allLabel, self.testData, self.testLabel
+                train_data = Variable(train_data.cuda().type(self.Tensor))
+                train_label = Variable(train_label.cuda().type(self.LongTensor))
+
+                tok, outputs = self.model(train_data)
+
+                loss = self.criterion_cls(outputs, train_label) 
+
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+
+
+            # out_epoch = time.time()
+
+
+            # test process
+            if (e + 1) % 1 == 0:
+                self.model.eval()
+                Tok, Cls = self.model(test_data)
+
+
+                loss_test = self.criterion_cls(Cls, test_label)
+                y_pred = torch.max(Cls, 1)[1]
+                acc = float((y_pred == test_label).cpu().numpy().astype(int).sum()) / float(test_label.size(0))
+                train_pred = torch.max(outputs, 1)[1]
+                train_acc = float((train_pred == train_label).cpu().numpy().astype(int).sum()) / float(train_label.size(0))
+
+                print('Epoch:', e,
+                      '  Train loss: %.6f' % loss.detach().cpu().numpy(),
+                      '  Test loss: %.6f' % loss_test.detach().cpu().numpy(),
+                      '  Train accuracy %.6f' % train_acc,
+                      '  Test accuracy is %.6f' % acc)
+
+                self.log_write.write(str(e) + "    " + str(acc) + "\n")
+                num = num + 1
+                averAcc = averAcc + acc
+                if acc > bestAcc:
+                    bestAcc = acc
+                    Y_true = test_label
+                    Y_pred = y_pred
+
+
+        torch.save(self.model.module.state_dict(), 'model.pth')
+        averAcc = averAcc / num
+        print('The average accuracy is:', averAcc)
+        print('The best accuracy is:', bestAcc)
+        self.log_write.write('The average accuracy is: ' + str(averAcc) + "\n")
+        self.log_write.write('The best accuracy is: ' + str(bestAcc) + "\n")
+
+        return bestAcc, averAcc, Y_true, Y_pred
+
+def main():
+    best = 0
+    aver = 0
+    result_write = open("./results/sub_result.txt", "w")
+
+    for i in range(9):
+        starttime = datetime.datetime.now()
+
+
+        seed_n = np.random.randint(2021)
+        print('seed is ' + str(seed_n))
+        random.seed(seed_n)
+        np.random.seed(seed_n)
+        torch.manual_seed(seed_n)
+        torch.cuda.manual_seed(seed_n)
+        torch.cuda.manual_seed_all(seed_n)
+
+
+        print('Subject %d' % (i+1))
+        exp = ExP(i + 1)
+
+        bestAcc, averAcc, Y_true, Y_pred = exp.train()
+        print('THE BEST ACCURACY IS ' + str(bestAcc))
+        result_write.write('Subject ' + str(i + 1) + ' : ' + 'Seed is: ' + str(seed_n) + "\n")
+        result_write.write('Subject ' + str(i + 1) + ' : ' + 'The best accuracy is: ' + str(bestAcc) + "\n")
+        result_write.write('Subject ' + str(i + 1) + ' : ' + 'The average accuracy is: ' + str(averAcc) + "\n")
+
+        endtime = datetime.datetime.now()
+        print('subject %d duration: '%(i+1) + str(endtime - starttime))
+        best = best + bestAcc
+        aver = aver + averAcc
+        if i == 0:
+            yt = Y_true
+            yp = Y_pred
+        else:
+            yt = torch.cat((yt, Y_true))
+            yp = torch.cat((yp, Y_pred))
+
+
+    best = best / 9
+    aver = aver / 9
+
+    result_write.write('**The average Best accuracy is: ' + str(best) + "\n")
+    result_write.write('The average Aver accuracy is: ' + str(aver) + "\n")
+    result_write.close()
+
+
+if __name__ == "__main__":
+    print(time.asctime(time.localtime(time.time())))
+    main()
+    print(time.asctime(time.localtime(time.time())))
